b269961abc33d2dbea0229f2138a07c41595ec98
wisdom <barkatimohamedamine@gmail.com>
2026-03-29 22:48:00 +0100

    add: head component

---
+++ public/components/head.php
@@ -0,0 +1,15 @@
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>AlgoSpark</title>
+    <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+    <link
+        rel="icon"
+        type="image/svg+xml"
+        href="/public/assets/media/svg-components/code-slash.svg" />
+    <link
+        rel="stylesheet"
+        href="/public/assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="/public/assets/css/style.css" />
+
+</head>
--- public/pages/problemset.php
+++ public/pages/problemset.php
@@ -1,20 +1,8 @@
 <!doctype html>
 <html lang="en">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>AlgoSpark</title>
-  <link
-    rel="icon"
-    type="image/svg+xml"
-    sizes="any"
-    href="/public/assets/media/svg-components/code-slash.svg" />
-  <link
-    rel="stylesheet"
-    href="/public/assets/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="/public/assets/css/style.css" />
-</head>
+<?php
+require_once __DIR__ . '/../components/head.php';
+?>
 
 <body>
   <header>
